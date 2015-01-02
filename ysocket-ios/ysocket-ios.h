73027b69a38d5d70cc7d3e6b4b9cd7bba560c486
彭运筹 <pengyunchou@pengyunchoudeMac-Pro.local>
2015-01-02 13:15:05 +0800

    add framework target

---
+++ ysocket-ios/ysocket-ios.h
@@ -0,0 +1,19 @@
+//
+//  ysocket-ios.h
+//  ysocket-ios
+//
+//  Created by 彭运筹 on 15/1/2.
+//  Copyright (c) 2015年 swift. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ysocket-ios.
+FOUNDATION_EXPORT double ysocket_iosVersionNumber;
+
+//! Project version string for ysocket-ios.
+FOUNDATION_EXPORT const unsigned char ysocket_iosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ysocket_ios/PublicHeader.h>
+
+
