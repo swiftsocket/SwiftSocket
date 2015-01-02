7e1ea2c549caa54dbcdaeeaf6433666f7381e87e
彭运筹 <pengyunchou@pengyunchoudeMac-Pro.local>
2015-01-02 13:17:52 +0800

    add osx framework

---
+++ ysocketOSX/ysocketOSX.h
@@ -0,0 +1,19 @@
+//
+//  ysocketOSX.h
+//  ysocketOSX
+//
+//  Created by 彭运筹 on 15/1/2.
+//  Copyright (c) 2015年 swift. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ysocketOSX.
+FOUNDATION_EXPORT double ysocketOSXVersionNumber;
+
+//! Project version string for ysocketOSX.
+FOUNDATION_EXPORT const unsigned char ysocketOSXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ysocketOSX/PublicHeader.h>
+
+
