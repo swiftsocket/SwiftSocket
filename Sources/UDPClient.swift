7295b73f620c39b0012e6a39af0cb23b6d444e27
Hubert Zhang <hubert.zyk@gmail.com>
2017-01-14 16:02:05 +0800

    Add recv for UDP Client
    Allow UDP Client receive response

--- Sources/UDPClient.swift
+++ Sources/UDPClient.swift
@@ -111,6 +111,31 @@ open class UDPClient: Socket {
         }
     }
     
+    //TODO add multycast and boardcast
+    open func recv(_ expectlen: Int) -> ([Byte]?, String, Int) {
+        if let fd = self.fd {
+            var buff: [Byte] = [Byte](repeating: 0x0,count: expectlen)
+            var remoteipbuff: [Int8] = [Int8](repeating: 0x0,count: 16)
+            var remoteport: Int32 = 0
+            let readLen: Int32 = c_yudpsocket_recive(fd, buff: buff, len: Int32(expectlen), ip: &remoteipbuff, port: &remoteport)
+            let port: Int = Int(remoteport)
+            var address = ""
+            if let ip = String(cString: remoteipbuff, encoding: String.Encoding.utf8) {
+                address = ip
+            }
+          
+            if readLen <= 0 {
+                return (nil, address, port)
+            }
+          
+            let rs = buff[0...Int(readLen-1)]
+            let data: [Byte] = Array(rs)
+            return (data, address, port)
+        }
+        
+        return (nil, "no ip", 0)
+    }
+    
     open func close() {
         guard let fd = self.fd else { return }
         
